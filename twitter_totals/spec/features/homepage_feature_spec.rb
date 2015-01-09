4e2f6ad3ffb0ac3b96036262285cdcb23583ab58
Yvette <yvettecook@gmail.com>
2015-01-09 14:52:05 +0000

    Homepage has input box

--- twitter_totals/spec/features/homepage_feature_spec.rb
+++ twitter_totals/spec/features/homepage_feature_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 describe 'On the homepage' do
 
   it 'there is a prompt to enter a twitter user name' do
-      visit '/'
-      expect(page).to have_content('Enter twitter username:')
+    visit '/'
+    expect(page).to have_content('Enter twitter username:')
+  end
+
+  it 'there is a box to enter a username' do
+    visit '/'
+    expect(page).to have_css('input')
   end
 
 end
