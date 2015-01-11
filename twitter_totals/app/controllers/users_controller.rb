3f51fd816a31235d7226be3590ab82c931919dd8
Yvette <yvettecook@gmail.com>
2015-01-11 12:18:52 +0000

    User show retreiving user.name - needed to make name a permitted parameter

--- twitter_totals/app/controllers/users_controller.rb
+++ twitter_totals/app/controllers/users_controller.rb
@@ -4,15 +4,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.create(params[:name])
+    @user = User.create(user_params)
     redirect_to user_path(@user)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
+  def user_params
+    params.require(:user).permit(:name)
+  end
 
 end
