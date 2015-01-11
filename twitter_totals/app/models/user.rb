14465b4e893660b6b818aa16788ebdfbc75c3983
Yvette <yvettecook@gmail.com>
2015-01-11 16:41:10 +0000

    After a lot of documentation pain, can get total number of own tweets since last 200th retweet

--- twitter_totals/app/models/user.rb
+++ twitter_totals/app/models/user.rb
@@ -10,4 +10,33 @@ class User < ActiveRecord::Base
     twitter_user.followers_count
   end
 
+  def following_count
+    twitter_user.friends_count
+  end
+
+  def tweets_count
+    twitter_user.statuses_count
+  end
+
+  def retweets_check
+    name = self.name
+    @all_retweets = $twitter.retweeted_by_user(name, options={:count => 200})
+    if @all_retweets.count < 200
+      @all_retweets.count
+    else
+      tweets_vs_retweets
+    end
+  end
+
+  def more_than_200_retweets
+    oldest_rt_id = @all_retweets.last.id
+    # oldest_rt_date = @all_retweets.last.created_at
+  end
+
+  def own_tweets_count
+    without_rt = $twitter.user_timeline('ynzc', options={:include_rts => false, :since => 364392548598427649, :count => 200})
+    without_rt.length
+  end
+
+
 end
--- twitter_totals/spec/models/user_spec.rb
+++ twitter_totals/spec/models/user_spec.rb
@@ -3,14 +3,30 @@ require 'rails_helper'
 describe 'Users' do
 
   let(:yvette) { User.create(name: 'ynzc') }
-
+  let(:camilla) { User.create(name: 'cnvk') }
 
   it 'have a name' do
     expect(yvette.name).to eq('ynzc')
   end
 
-  it 'can get information on an account' do
+  it 'can get number of followers for an account' do
     expect(yvette.followers_count).to eq(203)
+  end
+
+  it 'can get number of followering for an account' do
+    expect(yvette.following_count).to eq(534)
+  end
+
+  it 'can get total number of tweets' do
+    expect(yvette.tweets_count).to eq(505)
+  end
+
+  it 'can get total number of retweets' do
+    expect(camilla.retweets_check).to eq(2)
+  end
+
+  it 'can return total tweets since first retweet in range' do
+    expect(yvette.own_tweets_count).to eq(87)
   end
 
 end
