472ac686deb2a8f876da4a561f3501231d0d021d
Yvette <yvettecook@gmail.com>
2015-01-11 14:03:49 +0000

    API call in seperate method

--- twitter_totals/app/models/user.rb
+++ twitter_totals/app/models/user.rb
@@ -2,8 +2,12 @@ require 'twitter'
 
 class User < ActiveRecord::Base
 
+  def twitter_user
+    $twitter.user(self.name)
+  end
+
   def followers_count
-    return $twitter.user(self.name).followers_count
+    twitter_user.followers_count
   end
 
 end
