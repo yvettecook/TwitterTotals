602527290c5586f84e46098231eabed1e477bbc1
Yvette <yvettecook@gmail.com>
2015-01-12 21:29:56 +0000

    Out with the old, in with the new. Re-doing scoring system

--- twitter_totals/app/models/user.rb
+++ twitter_totals/app/models/user.rb
@@ -6,63 +6,17 @@ class User < ActiveRecord::Base
     $twitter.user(self.name)
   end
 
-  def followers_count
-    twitter_user.followers_count
+  def last_cent_tweets
+    last_cent_tweets ||= $twitter.user_timeline(name, options={:include_rts => true, :count => 100})
   end
 
-  def following_count
-    twitter_user.friends_count
+  def last_cent_tweet_id
+    last_cent_tweets.last.id
   end
 
-  def tweets_count
-    twitter_user.statuses_count
-  end
-
-  def retweets_count
-    name = self.name
-    @all_retweets = $twitter.retweeted_by_user(name, options={:count => 200})
-    return @all_retweets.count
-  end
-
-  def calculate_narcissism_score
-    if self.retweets_count < 100
-      score = self.low_RT_narcissism_score
-    else
-      score = self.high_RT_narcissim_score
-    end
-    return score.round(2)
-  end
-
-  def oldest_rt_in_range_id
-    oldest_id = @all_retweets.last.id
-  end
-
-  def own_tweets_count
-    name = self.name
-    last_retweet_id = self.oldest_rt_in_range_id
-    without_rt = $twitter.user_timeline(name, options={:include_rts => false, :since => last_retweet_id, :count => 200})
-    without_rt.length
-  end
-
-  def own_rt_tweets_count
-    name = self.name
-    last_retweet_id = self.oldest_rt_in_range_id
-    without_rt = $twitter.user_timeline(name, options={:include_rts => false, :since => last_retweet_id, :count => 200})
-    without_rt.length
-  end
-
-  def high_RT_narcissim_score
-    tweets = self.own_tweets_count
-    score = tweets / 100.0
-    return score
-  end
-
-  def low_RT_narcissism_score
-    all_tweets = $twitter.user_timeline(name, options={:include_rts => false, :count => 200})
-    last_tweet_id = all_tweets.last.id
-    retweets_since_last_tweet = $twitter.retweeted_by_user(name, options={:since => last_tweet_id, :count => 200})
-    retweets_since_count = retweets_since_last_tweet.length
-    return self.tweets_count / retweets_since_count
+  def non_rts_in_range
+    all_tweets = $twitter.user_timeline(name, options={:include_rts => false, :since => self.last_cent_tweet_id, :count => 100})
+    all_tweets.length
   end
 
 end
--- twitter_totals/spec/models/user_spec.rb
+++ twitter_totals/spec/models/user_spec.rb
@@ -10,43 +10,21 @@ describe 'Users' do
     expect(yvette.name).to eq('ynzc')
   end
 
-  it 'can get number of followers for an account' do
-    expect(yvette.followers_count).to eq(205)
-  end
+  context 'new trial scoring system' do
 
-  it 'can get number of following for an account' do
-    expect(yvette.following_count).to eq(534)
-  end
+    it 'can get last 100 tweets' do
+      expect(yvette.last_cent_tweets.length).to eq(100)
+    end
 
-  it 'can get total number of tweets' do
-    expect(yvette.tweets_count).to eq(505)
-  end
+    it 'can get id of last tweet in range' do
+      expect(yvette.last_cent_tweet_id).to eq(488712443284439040)
+    end
 
-  it 'can get total number of retweets' do
-    expect(camilla.retweets_count).to eq(2)
-  end
+    it 'can get number of non-RT since last cent tweet' do
+      expect(yvette.non_rts_in_range).to eq(48)
+    end
 
-  it 'can get id of oldest RT in range' do
-    yvette.retweets_count
-    expect(yvette.oldest_rt_in_range_id).to eq(364392548598427649)
-  end
 
-  it 'can return total tweets since first retweet in range' do
-    yvette.retweets_count
-    expect(yvette.own_tweets_count).to eq(87)
-  end
-
-  it 'can return total tweets since first retweet in range - for a different user' do
-    amal.retweets_count
-    expect(amal.own_tweets_count).to eq(171)
-  end
-
-  it 'can return a narcissism score out of 10 for someone with 200+ RTs' do
-    expect(yvette.calculate_narcissism_score).to eq(0.87)
-  end
-
-  it 'can return a narcissism score out of 10 for someone less than 200 RTs' do
-    expect(amal.calculate_narcissism_score).to eq(18.0)
   end
 
 end
