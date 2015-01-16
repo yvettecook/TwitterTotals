1a2e23f5e63178a387e6b9127bf141ecf0debc38
Yvette <yvettecook@gmail.com>
2015-01-16 12:23:09 +0000

    User working with concern - next step: twitter configured on initialization

--- twitter_totals/app/models/user.rb
+++ twitter_totals/app/models/user.rb
@@ -5,59 +5,54 @@ class User < ActiveRecord::Base
 
   include TwitterClient
 
-  # def initialize()
-  #   # @twitter = twitter
-  #   # @twitter = Twitter.instance
-  # end
-
   def twitter_client
     @twitter_client ||= self.twitter
   end
 
   def twitter_user
-    @twitter_user = twitter_client.user(self.name)
+    @twitter_user ||= twitter_client.user(self.name)
   end
 
   def fullname
     twitter_user.name
   end
-  #
-  # def narcissism_score
-  #   @narcissim_score ||= calculate_narcissism_score
-  # end
-  #
-  # def last_cent_tweets
-  #   last_cent_tweets ||= twitter_user.user_timeline(name, options={:include_rts => true, :count => 100})
-  # end
-  #
-  # def last_cent_tweet_id
-  #   last_cent_tweets.last.id
-  # end
-  #
-  # def non_rts
-  #   non_rts ||= @twitter.user_timeline(name, options={:include_rts => false, :since => self.last_cent_tweet_id, :count => 100})
-  # end
-  #
-  # def non_rts_in_range_count
-  #   non_rts.length
-  # end
-  #
-  # def rts_in_range_count
-  #   all_tweets = last_cent_tweets.count
-  #   all_tweets - non_rts_in_range_count
-  # end
-  #
-  # def calculate_retweet_percentage
-  #   tweets = non_rts_in_range_count
-  #   retweets = rts_in_range_count
-  #   total = (tweets + retweets) / 1.00
-  #   percentage = (retweets / total) * 100.0
-  # end
-  #
-  # def calculate_narcissism_score
-  #   diff_from_perfect = (50 - calculate_retweet_percentage).abs
-  #   points_diff = diff_from_perfect * 0.2
-  #   narcissism_score = 10 - points_diff
-  # end
+
+  def narcissism_score
+    @narcissim_score ||= calculate_narcissism_score
+  end
+
+  def last_cent_tweets
+    last_cent_tweets ||= twitter_client.user_timeline(name, options={:include_rts => true, :count => 100})
+  end
+
+  def last_cent_tweet_id
+    last_cent_tweets.last.id
+  end
+
+  def non_rts
+    non_rts ||= twitter_client.user_timeline(name, options={:include_rts => false, :since => self.last_cent_tweet_id, :count => 100})
+  end
+
+  def non_rts_in_range_count
+    non_rts.length
+  end
+
+  def rts_in_range_count
+    all_tweets = last_cent_tweets.count
+    all_tweets - non_rts_in_range_count
+  end
+
+  def calculate_retweet_percentage
+    tweets = non_rts_in_range_count
+    retweets = rts_in_range_count
+    total = (tweets + retweets) / 1.00
+    percentage = (retweets / total) * 100.0
+  end
+
+  def calculate_narcissism_score
+    diff_from_perfect = (50 - calculate_retweet_percentage).abs
+    points_diff = diff_from_perfect * 0.2
+    narcissism_score = 10 - points_diff
+  end
 
 end
--- twitter_totals/spec/models/user_spec.rb
+++ twitter_totals/spec/models/user_spec.rb
@@ -13,10 +13,6 @@ describe User do
       expect(yvette.say_hi).to eq("HI!")
     end
 
-    # it 'can authenticate with Twitter' do
-    #   expect(yvette.twitter_client).to eq({})
-    # end
-
     it 'have a name' do
       expect(yvette.name).to eq('ynzc')
     end
@@ -25,33 +21,33 @@ describe User do
       expect(yvette.fullname).to eq('Yvette')
     end
 
-    # it 'can get last 100 tweets' do
-    #   expect(yvette.last_cent_tweets.length).to eq(100)
-    # end
+    it 'can get last 100 tweets of a user' do
+      expect(yvette.last_cent_tweets.length).to eq(100)
+    end
 
-    # it 'can get id of last tweet in range' do
-    #   expect(yvette.last_cent_tweet_id).to eq(488712443284439040)
-    # end
-    #
-    # it 'can get number of non-RT since last 100th tweet' do
-    #   expect(yvette.non_rts_in_range_count).to eq(48)
-    # end
-    #
-    # it 'can get number of RT since last 100th tweet' do
-    #   expect(yvette.rts_in_range_count).to eq(52)
-    # end
-    #
-    # it 'can calculate percent of retweets' do
-    #   expect(yvette.calculate_retweet_percentage).to eq(52.0)
-    # end
-    #
-    # it 'can calculate a narcissism score' do
-    #   expect(yvette.calculate_narcissism_score).to eq(9.6)
-    # end
-    #
-    # it 'has method to report narcissism_score' do
-    #   expect(yvette.narcissism_score).to eq(9.6)
-    # end
+    it 'can get id of last tweet in range' do
+      expect(yvette.last_cent_tweet_id).to eq(488712443284439040)
+    end
+
+    it 'can get number of non-RT since last 100th tweet' do
+      expect(yvette.non_rts_in_range_count).to eq(48)
+    end
+
+    it 'can get number of RT since last 100th tweet' do
+      expect(yvette.rts_in_range_count).to eq(52)
+    end
+
+    it 'can calculate percent of retweets' do
+      expect(yvette.calculate_retweet_percentage).to eq(52.0)
+    end
+
+    it 'can calculate a narcissism score' do
+      expect(yvette.calculate_narcissism_score).to eq(9.6)
+    end
+
+    it 'has method to report narcissism_score' do
+      expect(yvette.narcissism_score).to eq(9.6)
+    end
 
   end
 
